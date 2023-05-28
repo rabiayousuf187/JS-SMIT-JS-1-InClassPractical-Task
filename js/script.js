25c9a9d34ecc94da6a0c8c2829d5ea0861bd5295
rabiayousuf187 <rabiayousuf187@gmail.com>
2023-05-28 20:18:27 +0500

    complete

--- js/script.js
+++ js/script.js
@@ -10,24 +10,49 @@ function exit(id){
 }
 
 function submit(){
+    let user = document.getElementById("user").value;
     let email = document.getElementById("email").value;
     let password = document.getElementById("password").value;
     let city = document.getElementById("city").value;
-    let country = document.getElementById("country").value;
+
 
     console.log("Email = ",email);
     console.log("password = ",password);
     console.log("city = ",city);
     console.log("country = ",country);
     
-    if( email !== "" && password !== "" && city !== "" && country !== ""){
+    // if( true){
+    if( user !== "" && email !== "" && password !== "" && city !== "" ){
+
+        if( city === "Karachi" || city === "Lahore" || city === "Islamabad" || city === "Multan" || city === "Hyderabad" ||city === "Peshawar" )
+        {
+            document.getElementById("country").value;
+            country.value = "Pakistan";
+        }
+        console.log("Form is Valid");
         document.getElementById("alert").style.display = "none";
         document.getElementById("msg-success").style.display = "block";
-        console.log("Form is Valid");
+        let head = document.getElementById("heading").getElementsByTagName("h1");
+        head[0].style.display = "none";
         setTimeout( ()=>{ 
             document.getElementById("banner").style.display = "none"; 
-            document.getElementById("body").style.background = "url('https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_640.jpg')"; 
-        }, 1200);
+            document.getElementById("form").style.display = "none"; 
+            document.getElementById("sub-btn").style.display = "none";
+            document.getElementById("msg-success").style.display = "none";
+            document.getElementById("body").className = "body"; 
+            
+        }, 500);
+        setTimeout( ()=> {
+            head[0].style.display = "block";
+            head[0].className += " head-change";
+            head[0].innerHTML = "Hi, " + user;
+        } , 500);
+            setTimeout( ()=> {
+                head[0].innerHTML = "Welcome to the Tech Show"
+            } , 4000)
+            setTimeout( ()=> {
+                head[0].innerHTML = "Have Fun!";
+            } , 6000);
         
         
         
